cfeec6027a885905a6cac8ba903119664cca74e1
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-04 19:16:59 +0900

    🐛 Fix : Store 엔티티 속성 수정

--- src/main/java/com/currency/turkey_express/global/base/entity/Store.java
+++ src/main/java/com/currency/turkey_express/global/base/entity/Store.java
@@ -1,4 +1,5 @@
 package com.currency.turkey_express.global.base.entity;
+
 import com.currency.turkey_express.global.base.enums.store.Category;
 import com.currency.turkey_express.global.base.enums.store.StoreStatus;
 import jakarta.persistence.Column;
@@ -13,10 +14,14 @@ import jakarta.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.sql.Time;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
-public class Store extends BaseEntity{
+@NoArgsConstructor
+public class Store extends BaseEntity {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,28 +31,29 @@ public class Store extends BaseEntity{
 
 	@Column(nullable = false)
 	private Time openTime;
+
 	@Column(nullable = false)
 	private Time closeTime;
 
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private StoreStatus storeStatus;
+
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private Category category;
 
+	@Column(nullable = false)
 	private BigDecimal orderAmount;
 
+	@Setter
 	@ManyToOne
 	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
-	public void setUser(User user){
-		this.user = user;
-	}
 
-	public Store(){}
-	public Store(String storeName, Time openTime, Time closeTime, StoreStatus storeStatus, Category category, BigDecimal orderAmount) {
+	public Store(String storeName, Time openTime, Time closeTime, StoreStatus storeStatus,
+		Category category, BigDecimal orderAmount) {
 		this.storeName = storeName;
 		this.openTime = openTime;
 		this.closeTime = closeTime;
@@ -55,5 +61,4 @@ public class Store extends BaseEntity{
 		this.category = category;
 		this.orderAmount = orderAmount;
 	}
-
 }
