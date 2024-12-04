7b2175f37fdb7d3456eb9ac223ce50324f35d215
j-hann <j.han77055@gmail.com>
2024-12-04 15:05:45 +0900

    ✨ Feat : entity 생성
    유저, 쿠폰, 쿠폰 리스트, 포인트 entity 생성 완료

--- src/main/java/com/currency/turkey_express/global/base/entity/User.java
+++ src/main/java/com/currency/turkey_express/global/base/entity/User.java
@@ -10,7 +10,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import lombok.Getter;
 
@@ -21,34 +20,29 @@ public class User extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id; //사용자 id
 
-	@NotBlank
 	@Column(name = "email", unique = true)
 	private String email; //이메일
 
-	@NotBlank
-	@Column(name = "user_nickname", unique = true)
+	@Column(name = "user_nickname")
 	private String userNickname; //닉네임
 
-	@NotBlank
-	@Column(name = "email", unique = true)
+	@Column(name = "password")
 	private String password; //비밀번호
 
 	@Enumerated(EnumType.STRING)
-	@Column(name = "user_type", unique = true)
+	@Column(name = "user_type")
 	private UserType userType; //유저 타입
 
 	@Enumerated(EnumType.STRING)
-	@Column(name = "user_status", unique = true)
+	@Column(name = "user_status")
 	private UserStatus userStatus; //유저 상태
 
-	@NotBlank
-	@Column(name = "leaved_at", unique = true)
+	@Column(name = "leaved_at")
 	private LocalDateTime leavedAt; //탈퇴일
 
-	@NotBlank
-	@Column(name = "total_point", unique = true)
+	@Column(name = "total_point")
 	private Integer totalPoint; //총합 포인트
 
 
