6e20389640d0c4ddd8d463874a75ea3dabb418f4
박예진 <qkrdpwls2002>
2024-12-05 11:11:41 +0900

    ✨ Feat : 가게 단건 조회 api 작성
    - 매커니즘 상 필요한 menu 의 Repository, Dto 부분 추가했습니다.

---
+++ src/main/java/com/currency/turkey_express/domain/store/dto/StoreMenuResponseDto.java
@@ -0,0 +1,30 @@
+package com.currency.turkey_express.domain.store.dto;
+
+import com.currency.turkey_express.domain.menu.dto.MenuInStoreResponsDto;
+import com.currency.turkey_express.global.base.entity.Store;
+import com.currency.turkey_express.global.base.enums.store.Category;
+import java.math.BigDecimal;
+import java.sql.Time;
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class StoreMenuResponseDto {
+	private String storeName;
+	private Time openTime;
+	private Time closeTime;
+	private BigDecimal orderAmount;
+	private Category category;
+	private Long favoriteCounts;
+	private List<MenuInStoreResponsDto> menus;
+
+	public StoreMenuResponseDto(Store store, Long favoriteCounts, List<MenuInStoreResponsDto> menus) {
+		this.storeName = store.getStoreName();
+		this.openTime = store.getOpenTime();
+		this.closeTime = store.getCloseTime();
+		this.orderAmount = store.getOrderAmount();
+		this.category = store.getCategory();
+		this.favoriteCounts = favoriteCounts;
+		this.menus = menus;
+	}
+}
--- src/main/java/com/currency/turkey_express/domain/store/repository/StoreRepository.java
+++ src/main/java/com/currency/turkey_express/domain/store/repository/StoreRepository.java
@@ -1,11 +1,15 @@
 package com.currency.turkey_express.domain.store.repository;
 
+import com.currency.turkey_express.domain.store.dto.StoreResponseDto;
 import com.currency.turkey_express.global.base.entity.Store;
 import com.currency.turkey_express.global.base.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
 	//jPQL 사용해서 유저의 가게 개수를 count 합니다
@@ -13,4 +17,22 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 		"SELECT COUNT(s) FROM Store AS s WHERE s.user = :user"
 	)
 	int findAllByUserId(@Param("user") User user);
+
+//	@Query(
+//		"SELECT new com.currency.turkey_express.domain.store.dto.StoreResponseDto(s) "+
+//			"FROM Store AS s "+
+//			"WHERE s.storeStatus = 'OPEN' "+
+//			"AND (:name IS NULL OR s.storeName LIKE %:name%) "+
+//			"AND (:category IS NULL OR s.category = :category) "+
+//			"AND (:minReviews IS NULL OR )"
+//	)
+//	List<StoreResponseDto> findByFilters(String name, String category, Long minReviews, Double minRating);
+
+	@Query(
+		"SELECT new com.currency.turkey_express.domain.store.dto.StoreResponseDto(s) "+
+			"FROM Store AS s WHERE s.storeStatus = 'OPEN'"
+	)
+	List<StoreResponseDto> findAllStores();
+
+
 }
