08617e8669ceeb484138492955d72b96de9a1b8f
박예진 <qkrdpwls2002>
2024-12-05 14:09:48 +0900

    ✨ Feat : 가게 전체 조회 api 작성
    - 쿼리 조인으로 진행

--- src/main/java/com/currency/turkey_express/domain/store/repository/StoreRepository.java
+++ src/main/java/com/currency/turkey_express/domain/store/repository/StoreRepository.java
@@ -3,6 +3,7 @@ package com.currency.turkey_express.domain.store.repository;
 import com.currency.turkey_express.domain.store.dto.StoreResponseDto;
 import com.currency.turkey_express.global.base.entity.Store;
 import com.currency.turkey_express.global.base.entity.User;
+import com.currency.turkey_express.global.base.enums.store.Category;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,15 +19,18 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 	)
 	int findAllByUserId(@Param("user") User user);
 
-//	@Query(
-//		"SELECT new com.currency.turkey_express.domain.store.dto.StoreResponseDto(s) "+
-//			"FROM Store AS s "+
-//			"WHERE s.storeStatus = 'OPEN' "+
-//			"AND (:name IS NULL OR s.storeName LIKE %:name%) "+
-//			"AND (:category IS NULL OR s.category = :category) "+
-//			"AND (:minReviews IS NULL OR )"
-//	)
-//	List<StoreResponseDto> findByFilters(String name, String category, Long minReviews, Double minRating);
+	@Query(
+		"SELECT new com.currency.turkey_express.domain.store.dto.StoreResponseDto(s) "+
+			"FROM Store AS s "+
+			"LEFT JOIN Review r ON r.order.store.id = s.id "+
+			"WHERE s.storeStatus = 'OPEN' "+
+			"AND (:name IS NULL OR s.storeName LIKE %:name%) "+
+			"AND (:category IS NULL OR s.category = :category) "+
+			"GROUP BY s.id "+
+			"HAVING (:minReviews IS NULL OR COUNT(r) >= :minReviews) "+
+			"AND (:minRating IS NULL OR AVG(r.point) >= :minRating)"
+	)
+	List<StoreResponseDto> findByFilters(String name, Category category, Long minReviews, Double minRating);
 
 	@Query(
 		"SELECT new com.currency.turkey_express.domain.store.dto.StoreResponseDto(s) "+
