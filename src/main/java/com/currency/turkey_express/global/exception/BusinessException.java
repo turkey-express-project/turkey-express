39eb10291fc4fbde097ed364cc37a377d0232bcc
j-hann <j.han77055@gmail.com>
2024-12-05 10:22:18 +0900

    ✨ Feat : 회원가입 기능, 예외처리 추가
    회원가입 api 구현, globalExceptionHandle, ExceptionType 추가

---
+++ src/main/java/com/currency/turkey_express/global/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package com.currency.turkey_express.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+	private ExceptionType exceptionType;
+
+	public BusinessException(ExceptionType exceptionType) {
+		this.exceptionType = exceptionType;
+	}
+}
