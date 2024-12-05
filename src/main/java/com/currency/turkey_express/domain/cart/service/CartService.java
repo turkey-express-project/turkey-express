e569b379350689ca8dfb15fa5e2ca6d25f3ad558
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-05 15:58:02 +0900

    ✨ Feat : 장바구니 담기 API Service 추가

---
+++ src/main/java/com/currency/turkey_express/domain/cart/service/CartService.java
@@ -0,0 +1,48 @@
+package com.currency.turkey_express.domain.cart.service;
+
+import com.currency.turkey_express.domain.cart.dto.CartMenuResponseDto;
+import com.currency.turkey_express.domain.cart.dto.CartRequestDto;
+import com.currency.turkey_express.domain.cart.dto.MenuOptionSet;
+import com.currency.turkey_express.domain.cart.repository.MenuRepository;
+import com.currency.turkey_express.domain.cart.repository.MenuSubCategoryRepository;
+import com.currency.turkey_express.domain.cart.repository.MenuTopCategoryRepository;
+import com.currency.turkey_express.global.base.entity.Menu;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+
+	private final MenuRepository menuRepository;
+
+	private final MenuTopCategoryRepository menuTopCategoryRepository;
+
+	private final MenuSubCategoryRepository menuSubCategoryRepository;
+
+
+	@Transactional(readOnly = true)
+	public CartMenuResponseDto getSelectedMenuInfo(
+		CartRequestDto cartRequestDto
+	) {
+		Menu menu = menuRepository.findById(cartRequestDto.getMenuId())
+			.orElseThrow(() -> new RuntimeException("존재하지 않는 메뉴 입니다."));
+
+		List<MenuOptionSet> menuOptionSets =
+			cartRequestDto.getMenuOptions().stream()
+				.map(optionSet -> {
+					return new MenuOptionSet(
+						menuTopCategoryRepository.findById(optionSet.getTopCategoryId())
+							.orElseThrow(() -> new RuntimeException("존재하지 않는 상위 옵션입니다."))
+						,
+						menuSubCategoryRepository.findById(optionSet.getSubCategoryId())
+							.orElseThrow(() -> new RuntimeException("존재하지 않는 하위 옵션입니다."))
+					);
+				})
+				.toList();
+
+		return new CartMenuResponseDto(menu, menuOptionSets);
+	}
+}
