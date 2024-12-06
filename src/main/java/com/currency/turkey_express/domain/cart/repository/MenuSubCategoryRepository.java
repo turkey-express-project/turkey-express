824c4d8cd4f2a9895b922e1ba2d1fe353db774b6
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-06 10:42:59 +0900

    🐛 Fix : Mege 충돌 수정

--- src/main/java/com/currency/turkey_express/domain/cart/repository/MenuSubCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.currency.turkey_express.domain.cart.repository;
-
-import com.currency.turkey_express.global.base.entity.MenuSubCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuSubCategoryRepository extends JpaRepository<MenuSubCategory, Long> {
-
-}
--- src/main/java/com/currency/turkey_express/domain/cart/repository/MenuTopCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.currency.turkey_express.domain.cart.repository;
-
-import com.currency.turkey_express.global.base.entity.MenuTopCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuTopCategoryRepository extends JpaRepository<MenuTopCategory, Long> {
-
-}
