3fa2dda256ac71b62a99b8646e4ddf597f1abb76
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-06 20:38:42 +0900

    ♻️ Refactor : 컨버터 코드 리팩토링

--- src/main/java/com/currency/turkey_express/global/config/WebConfig.java
+++ src/main/java/com/currency/turkey_express/global/config/WebConfig.java
@@ -3,23 +3,27 @@ package com.currency.turkey_express.global.config;
 import com.currency.turkey_express.global.converter.CookieCartDataConverter;
 import com.currency.turkey_express.global.interceptor.LoginInterceptor;
 import com.currency.turkey_express.global.interceptor.UserInterceptor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
 	private final LoginInterceptor loginInterceptor;
 	private final UserInterceptor userInterceptor;
 	private final CookieCartDataConverter cookieCartDataConverter;
-//	public WebConfig(LoginInterceptor loginInterceptor, UserInterceptor userInterceptor) {
-//		this.loginInterceptor = loginInterceptor;
-//		this.userInterceptor = userInterceptor;
-//	}
+
+	public WebConfig(
+		LoginInterceptor loginInterceptor,
+		UserInterceptor userInterceptor,
+		CookieCartDataConverter cookieCartDataConverter
+	) {
+		this.loginInterceptor = loginInterceptor;
+		this.userInterceptor = userInterceptor;
+		this.cookieCartDataConverter = cookieCartDataConverter;
+	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
