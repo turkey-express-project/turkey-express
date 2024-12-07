6f937eb0e520dc8b18da73b823a8a1dcbe044c75
j-hann <j.han77055@gmail.com>
2024-12-07 15:24:02 +0900

    ✨ Feat : 로그인시 totalPoint 자동으로 업데이트, 유저 단건 조회 기능 추가
    로그인시 totalPoint 필드 데이터 자동으로 업데이트, 마이페이지 유저 단건 조회 기능 추가

--- src/main/java/com/currency/turkey_express/domain/user/service/UserService.java
+++ src/main/java/com/currency/turkey_express/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.currency.turkey_express.domain.user.service;
 
+import com.currency.turkey_express.domain.point.repository.PointRepository;
 import com.currency.turkey_express.domain.user.dto.UserDeleteRequestDto;
 import com.currency.turkey_express.domain.user.dto.UserResponseDto;
 import com.currency.turkey_express.domain.user.repository.UserRepository;
@@ -10,21 +11,24 @@ import com.currency.turkey_express.global.config.PasswordEncoder;
 import com.currency.turkey_express.global.exception.BusinessException;
 import com.currency.turkey_express.global.exception.ExceptionType;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final PointRepository pointRepository;
 
 	/**
 	 * 회원가입 API
-	 * TODO 회원가입 유효성 검사 예외처리 하기
 	 */
 	@Transactional
 	public UserResponseDto signUp(String email, String userNickname, String password,
@@ -47,7 +51,7 @@ public class UserService {
 
 	/**
 	 * 로그인 API
-	 * TODO 이메일 유효성 검사 예외처리 하기
+	 * - 로그인시 totalPoint 자동으로 업데이트
 	 */
 	public User login(String email, String password) {
 
@@ -63,6 +67,18 @@ public class UserService {
 		if (!passwordEncoder.matches(password, user.getPassword())) {
 			throw new BusinessException(ExceptionType.PASSWORD_NOT_MATCH);
 		}
+
+		//유저 포인트 합산하기
+		BigDecimal totalPoints = pointRepository.getTotalPointsByUserId(user.getId());
+
+		//포인트 업데이트
+		user.updateTotalPoint(totalPoints);
+
+		//업데이트된 user 저장
+		userRepository.save(user);
+
+		//포인트 로그 확인
+		log.info("totalPoints : {}", totalPoints);
 
 		return user;
 	}
@@ -99,4 +115,18 @@ public class UserService {
 		return new UserResponseDto(user);
 	}
 
+	/**
+	 * 유저 단건 조회 API
+	 */
+	public UserResponseDto getUser(Long userId, Long loginUserId) {
+		//사용자 id 확인
+		User user = userRepository.findByIdOrElseThrow(userId);
+
+		//로그인한 사용자랑 ID가 일치하는지 확인
+		if (!userId.equals(loginUserId)) {
+			throw new BusinessException(ExceptionType.USER_NOT_MATCH);
+		}
+
+		return UserResponseDto.toDto(user);
+	}
 }
